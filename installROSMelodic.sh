a89bc6add1e2c680d17ba02b9b2b06731751683f
Interactics <zz31@naver.com>
2020-05-05 14:51:04 +0900

    bug fix

--- installROSMelodic.sh
+++ installROSMelodic.sh
@@ -13,46 +13,58 @@ read
 
 ROS_VER=${ROS_VER:="melodic"}
 NAME_CATKIN_WS=${NAME_CATKIN_WS:="catkin_ws"}
+
+#Add Repository
+sudo apt-add-repository universe
+sudo apt-add-repository multiverse
+sudo apt-add-repository restricted
+
 #Setup source.list and keys
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 #Setup keys
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-#Install
+
+
+#Installation
 echo "Updating apt"
 sudo apt update -y
-sudo apt upgrade -y
+
 echo "Install ros-melodic-desktop-full"
 sudo apt install -y ros-melodic-desktop-full
 
 #initilaize rosdep and update
 sudo sh -c "rosdep init"
+sudo apt-get install python-rosdep -y
+sudo rosdep init
 rosdep update
+
+grep -q -F 'source /opt/ros/melodic/setup.bash' ~/.bashrc || echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 
 #Env Setting"
 echo "Environment setup"
 source $HOME/.bashrc
 
+
 #Install Dep for catkin-tools
 echo "Install Catkin tools dependencies"
 sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential 
-sudo apt install -y python-pip
 
-#Create catkin_ws and build 
-echo "Create Catkin_ws dir and Build"
+
+#Create catkin_ws 
+echo "Create Catkin_ws Dir"
 mkdir -p $HOME/$NAME_CATKIN_WS/src
-cd $HOME/$NAME_CATKIN_WS/
+cd $HOME/$NAME_CATKIN_WS/src
 catkin_init_workspace
-source $HOME/.bashrc
-
 cd $HOME/$NAME_CATKIN_WS
 catkin_make
 
+
 #Set Source
-sh -c "echo \"source /opt/ros/$ROS_VER/setup.bash\" >> ~/.bashrc"
-sh -c "echo \"source ~/$NAME_CATKIN_WS/devel/setup.bash\" >> ~/.bashrc"
+grep -q -F 'source ~/catkin_ws/devel/setup.bash' ~/.bashrc || echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+
 
 #Set ROS Network
-sh -c "echo \"export ROS_HOSTNAME=localhost" >> ~/.bashrc"
+sh -c "echo \"export ROS_HOSTNAME=localhost" >> ~/.bashrc
 sh -c "echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/.bashrc"
 
 #Set Aalias command
